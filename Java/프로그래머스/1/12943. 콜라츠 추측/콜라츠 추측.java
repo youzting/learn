7b2dfcfeb1c7f091e89e2c977439a2cc68d08b7c
youzting <158013348+youzting@users.noreply.github.com>
2026-02-25 09:09:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num) {
+        long n = num;   
+        int count = 0;
+
+        while (n != 1 && count < 500) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            count++;
+        }
+
+        return (n == 1) ? count : -1;
+    }
+}
